32e12f93de87573d26cc606f449d93563c6e6d4f
enji7 <enji@bounty>
2021-02-18 08:14:25 +0100

    fix for Payara

--- src/main/java/systems/enji/demo/mp/metrics/ApplicationConfig.java
+++ src/main/java/systems/enji/demo/mp/metrics/ApplicationConfig.java
@@ -1,9 +1,17 @@
 package systems.enji.demo.mp.metrics;
 
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("")
 public class ApplicationConfig extends Application {
+
+  @Override
+  public Set<Class<?>> getClasses() {
+    // Payara (Jersey) has problems with REST interface classes, so we have to define the implementation class explicitly:
+    return Set.of(DemoResource.class, GaugeResource.class, YetAnotherDemoResource.class);
+  }
 
 }
